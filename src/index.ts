486d9a438e61edb4719630916e9ce49ae8d70295
shicks1080 <stuart.hicks@gmail.com>
2020-07-14 17:15:24 +0200

    Adding a map to the screen
    Adding a map to the index.html plus adding an instance of a map with some properties to the index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,11 @@ const company = new Company()
 
 console.log(user)
 console.log(company)
+
+new google.maps.Map(document.getElementById('map'), {
+	center: {
+		lat: 0,
+		lng: 0,
+	},
+	zoom: 1,
+})
