8882d1011ab1735ecd02dca4bd1b971323ff9683
shicks1080 <stuart.hicks@gmail.com>
2020-07-14 09:59:22 +0200

    Initial commit
    Actually a little way into the project tut.

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { User } from './components/User'
+import { Company } from './components/Company'
+
+import {} from 'googlemaps'
+
+const user = new User()
+const company = new Company()
+
+console.log(user)
+console.log(company)
